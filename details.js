82672a66027a7737c701b0b995869b4bfe04f868
wally <waldifiaga8@gmail.com>
2023-12-02 18:17:58 +0100

    🚑🔥 ff d'une partie du code
    trop compliqué pour la date de rendu mais j'abandonne pas, après la date de rendu je reprends mes idées

--- details.js
+++ details.js
@@ -1,11 +1,4 @@
-// document.querySelectorAll('.planete').forEach((planete) => {
-//     planete.addEventListener('click', (event) => {
-//         const contenu = event.target.querySelector('.contenu');
-//         if (contenu) {
-//             contenu.style.display = 'block';
-//         }
-//     });
-// });
+
 
 const planete = document.querySelector('.planete');
 // Obtenez les styles calculés pour l'élément '.planete'
@@ -22,70 +15,40 @@ const videoDiv = document.querySelector('.video');
 const threeDiv = document.querySelector('.three');
 // const div = document.createElement('div');
 
-// document.querySelectorAll('.planete').forEach((planete) => {
-//     planete.addEventListener('click', () => {
-//         document.querySelectorAll('.planete .contenu').forEach((contenu) => {
-//             if (contenu.style.opacity === '0') {
-//                 contenu.style.opacity = '100';
-//                 console.log("affiche contenu");
-//                 planete.style.width = '100vw';
-//                 planete.style.height = '100vh';
-//                 planete.style.borderRadius = '0';
-//             } else {
-//                 contenu.style.opacity = '0';
-//                 console.log("cache contenu");
-//                 planete.style.width = initialWidth;
-//                 planete.style.height = initialWidth;
-//                 planete.style.borderRadius = '100%';
-//             }
-//         });
-//     });
-// });
 
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data[0].div1.forEach(element => {
-            console.log("code ok");
-            codeDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        });
-        data[0].div2.forEach(element => {
-            console.log("sons ok");
-            sonsDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        });
-        // data[1].div1.forEach(element => {
-        //     console.log("graph ok");
-        //     graphDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        // });
-        // data[1].div2.forEach(element => {
-        //     console.log("design ok");
-        //     designDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        // });
-        // data[2].div1.forEach(element => {
-        //     console.log("video ok");
-        //     videoDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        // });
-        // data[2].div2.forEach(element => {
-        //     console.log("3D ok");
-        //     threeDiv.innerHTML += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p></div>`;
-        // });
-    })
-    .catch(error => console.error('Erreur attrapée :', error));
+// fetch('data.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         data[0].div1.forEach(element => {
+//             let contenu = "";
+//             contenu += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p>`;
+//             if (element.membres.length > 0) {
+//                 element.membres.forEach(membre => {
+//                     contenu += `<a href="${membre.lkdn}">${membre.name}</a>`;
+//                 });
+//             }
+//             contenu += `</div>`;
+//             codeDiv.innerHTML += contenu;
+//         });
+//     })
+//     .catch(error => console.error('Erreur attrapée :', error));
+
+
 
 document.querySelectorAll('.planete').forEach((planete) => {
     planete.addEventListener('click', () => {
         document.querySelectorAll('.planete').forEach((allPlanete) => {
             const contenu = allPlanete.querySelector('.contenu');
-            if (contenu.style.opacity === '0') {
-                contenu.style.opacity = '100';
+            if (contenu.style.display === 'none') {
+                contenu.style.display = 'block';
                 allPlanete.style.width = '100vw';
                 allPlanete.style.height = '100vh';
                 allPlanete.style.borderRadius = '0';
                 allPlanete.style.background = 'linear-gradient(1deg, #' + allPlanete.id + ' 0%, rgba(67, 66, 80, 0) 94%)';
                 console.log("affiche contenu");
             } else {
-                contenu.style.opacity = '0';
+                contenu.style.display = 'none';
                 allPlanete.style.width = initialWidth;
                 allPlanete.style.height = initialWidth;
                 allPlanete.style.borderRadius = '100%';
