c001b83e59b33b6879c626d2082d55829e8babfc
wally <waldifiaga8@gmail.com>
2023-12-11 21:55:07 +0100

    ✨ça y est jsuis de retours
    réintégration de fonctionnalité: retours de l'insertion des données par JS, je vais en dev

--- details.js
+++ details.js
@@ -7,32 +7,32 @@ const computedStyles = window.getComputedStyle(planete);
 const initialWidth = computedStyles.width;
 console.log("initialWidth : " + initialWidth);
 
-const codeDiv = document.querySelector('.code');
-const sonsDiv = document.querySelector('.sons');
+const codeDiv = document.querySelector('#code .flexzone');
+const sonsDiv = document.querySelector('#sons .flexzone');
 const graphDiv = document.querySelector('.graph');
 const designDiv = document.querySelector('.design');
 const videoDiv = document.querySelector('.video');
 const threeDiv = document.querySelector('.three');
 // const div = document.createElement('div');
 
-
-// fetch('data.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         data[0].div1.forEach(element => {
-//             let contenu = "";
-//             contenu += `<div class='creation' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p>`;
-//             if (element.membres.length > 0) {
-//                 element.membres.forEach(membre => {
-//                     contenu += `<a href="${membre.lkdn}">${membre.name}</a>`;
-//                 });
-//             }
-//             contenu += `</div>`;
-//             codeDiv.innerHTML += contenu;
-//         });
-//     })
-//     .catch(error => console.error('Erreur attrapée :', error));
+//creer une variable contenue que je vais remplir au fur et a mesure de donnée provenant de mon json pour tout mettre directement dans une balise class "flexzone"
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data[0].div1.forEach(element => {
+            let contenu = "";
+            contenu += `<div class='projet' id=${element.name}><h2>${element.name}</h2><p>${element.description}</p>`;
+            if (element.membres.length > 0) {
+                element.membres.forEach(membre => {
+                    contenu += `<a href="${membre.lkdn}">${membre.name}</a>`;
+                });
+            }
+            contenu += `</div>`;
+            codeDiv.innerHTML += contenu;
+        });
+    })
+    .catch(error => console.error('Erreur attrapée :', error));
 
 
 
