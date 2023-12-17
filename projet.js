9240761bb49c7f723b8dcbc287b1397c155536a6
wally <waldifiaga8@gmail.com>
2023-12-17 19:15:10 +0100

    🐛 affichafe de photo reglé
    les projets ont des images !

--- projet.js
+++ projet.js
@@ -5,13 +5,26 @@ let data;
 fetch('projet.json')
     .then((response) => response.json())
     .then((json) => {
+        
         let data = json;
         data = data[url];
+        console.log(json)
         console.log(data)
-        document.querySelector('body .projet .image').innerHTML += "<img src=" + data["image"] +" alt='image projet' >"
+       
+        let imageElement = document.querySelector('body .projet .image');
+        if (data["image"]) {
+            if (data["image"]>1) {
+                imageElement.innerHTML += "<img src=" + data["image"][0] +" alt='image projet' >";
+                imageElement.innerHTML += "<img src=" + data["image"][1] +" alt='image projet' >";
+            } else {
+            imageElement.innerHTML += "<img src=" + data["image"] +" alt='image projet' >";
+            }
+        } else {
+            imageElement.innerHTML += "<p>Image non disponible<br>désolé ;p</p>";
+        }
         document.querySelector('body .projet .details').innerHTML += "<h1 class='name'>" + data["name"] + "</h1>"
         document.querySelector('body .projet .details').innerHTML += "<div class='texte'><p>" + data["description"] + "</p></div>"
-        document.querySelector('body .container .texte').innerHTML += "<a href=" + data["url"] + ">" + "voir le projet </a>"
+        document.querySelector('body .projet .details').innerHTML += "<a href=" + data["url"] + ">" + "voir le projet </a>"
         // for (const i in data["technos"]) {
         //         const techno = data["technos"][i];
         //         const lien_techno = data["links"][i];
